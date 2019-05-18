c98ab0a4995fb78bc7e22a178e805d40b73b6bec
domw30 <domw30@gmail.com>
2019-05-18 19:22:04 +0100

    add event delegation

--- script.js
+++ script.js
@@ -51,10 +51,8 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodos: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodos(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodos: function(position) {
+    todoList.deleteTodos(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -84,8 +82,30 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
+      todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
     }
+  },
+  createDeleteButton: function() {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
+  },
+  setUpEventListeners: function() {
+    var todosUl = document.querySelector('ul');
+
+    todosUl.addEventListener('click', function(event) {
+
+      var elementClicked = event.target;
+
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodos(parseInt(elementClicked.parentNode.id));
+      }
+    });
   }
 };
+
+view.setUpEventListeners();
