df381daba98c643a5b3a98f61cf9ea6c0422fafb
domw30 <domw30@gmail.com>
2019-05-18 14:53:20 +0100

    add toggleAll function

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    if(this.todos.length === 0) {
+    if (this.todos.length === 0) {
       console.log('Yes hustler, your todo list is empty!')
     } else {
       console.log('My Makers Todo List:');
       for (var i = 0; i < this.todos.length; i++) {
         if (this.todos[i].completed === true) {
-          console.log('(x)',this.todos[i].todoText);
+          console.log('(✅)',this.todos[i].todoText);
         } else {
           console.log('( )',this.todos[i].todoText);
         }
@@ -32,6 +32,25 @@ var todoList = {
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
     this.displayTodos();
   }
 };
